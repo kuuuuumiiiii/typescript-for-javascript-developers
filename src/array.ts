38de979df794748337791374cc026c8db207572f
kuuuuumiiiii <kuuuuumiiiii@users.noreply.github.com>
2025-01-15 06:48:13 +0900

    基本的な型について

---
+++ src/array.ts
@@ -0,0 +1,14 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+let number2 : Array<number> = [1, 2, 3];
+
+let strings: string[] = ["TypeScript", "JavaScript", "CoffeeScript"];
+
+let nigigenHairetu: number[][] = [
+  [50, 100],
+  [150, 300]
+];
+
+let hairetu: (string | number | boolean)[] = [1, false, 'Japan'];
---
+++ src/void.ts
@@ -0,0 +1,7 @@
+export {};
+
+function rerurnNothing(): void {
+  console.log("I don't return anything!");
+}
+
+console.log(rerurnNothing());
